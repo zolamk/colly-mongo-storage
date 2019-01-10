a3018440f25e09bc25cde4bf5f8180e318df60fc
Zelalem <zola@programmer.net>
2019-01-10 17:35:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# colly-mongo-storage
+A MongoDB storage back end for the Colly web crawling/scraping framework https://go-colly.org
+
+Example Usage:
+
+```go
+package main
+
+import (
+	"log"
+
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/extensions"
+	"github.com/zolamk/colly-mongo-storage/colly/mongo"
+)
+
+func main() {
+	// Instantiate collector
+	c := colly.NewCollector()
+
+	storage := &mongo.Storage{
+		Database: "colly",
+		URI:      "mongodb://127.0.0.1:27017",
+	}
+
+	if err := c.SetStorage(storage); err != nil {
+        panic(err)
+    }
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		q.AddURL(e.Request.AbsoluteURL(e.Attr("href")))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		log.Println(r.Request.URL, "\t", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println(r.Request.URL, "\t", r.StatusCode, "\nError:", err)
+    })
+    
+    c.Visit("https://www.example.com")
+    
+}
+
+
+```
---
+++ colly/mongo/mongo.go
@@ -0,0 +1,117 @@
+package mongo
+
+import (
+	"context"
+	"log"
+	"net/url"
+	"strconv"
+
+	"github.com/mongodb/mongo-go-driver/mongo"
+
+	"github.com/mongodb/mongo-go-driver/x/bsonx"
+)
+
+type Storage struct {
+	Database string
+	URI      string
+	client   *mongo.Client
+	db       *mongo.Database
+	visited  *mongo.Collection
+	cookies  *mongo.Collection
+}
+
+func (s *Storage) Init() error {
+
+	var err error
+
+	if s.client, err = mongo.NewClient(s.URI); err != nil {
+
+		return err
+
+	}
+	if err = s.client.Connect(context.Background()); err != nil {
+
+		return err
+
+	}
+
+	s.db = s.client.Database(s.Database)
+
+	s.visited = s.db.Collection("colly_visited")
+
+	s.cookies = s.db.Collection("colly_cookies")
+
+	return nil
+
+}
+
+func (s *Storage) Visited(requestID uint64) error {
+
+	_, err := s.visited.InsertOne(context.Background(), bsonx.MDoc{
+		"requestID": bsonx.String(strconv.FormatUint(requestID, 10)),
+		"visited":   bsonx.Boolean(true),
+	})
+
+	return err
+
+}
+
+func (s *Storage) IsVisited(requestID uint64) (bool, error) {
+
+	result := bsonx.MDoc{}
+
+	err := s.visited.FindOne(nil, bsonx.MDoc{
+		"requestID": bsonx.String(strconv.FormatUint(requestID, 10)),
+	}).Decode(&result)
+	if err != nil {
+
+		if err == mongo.ErrNoDocuments {
+
+			return false, nil
+
+		}
+
+		log.Println(err)
+
+		return false, err
+
+	}
+
+	return true, nil
+
+}
+
+func (s *Storage) Cookies(u *url.URL) string {
+
+	result := bsonx.MDoc{}
+
+	if err := s.cookies.FindOne(nil, bsonx.MDoc{
+		"host": bsonx.String(u.Host),
+	}).Decode(&result); err != nil {
+
+		if err != mongo.ErrNoDocuments {
+
+			log.Println(err)
+
+		}
+
+		return ""
+
+	}
+
+	return result["cookies"].String()
+
+}
+
+func (s *Storage) SetCookies(u *url.URL, cookies string) {
+
+	if _, err := s.cookies.InsertOne(nil, bsonx.MDoc{
+		"host":    bsonx.String(u.Host),
+		"cookies": bsonx.String(cookies),
+	}); err != nil {
+
+		log.Println(err)
+
+	}
+
+}
